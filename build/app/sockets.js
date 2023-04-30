02aab0a8275c2a5b188343a85fc741ae9fa7bd2f
duongpham17 <duongpham17@icloud.com>
2023-05-01 00:23:23 +0100

    first commit

--- build/app/sockets.js
+++ build/app/sockets.js
@@ -1,13 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const sockets_1 = __importDefault(require("../sockets"));
 const socket = (app) => {
     const PORT = process.env.PORT || 8000;
     const development = process.env.NODE_ENV === "development";
-    app.listen(PORT, () => development && console.log(`http://localhost:${PORT}`));
-    // const server = createServer(app);
-    // const io = new Server(server);
-    // io.on('connection', (socket) => sockets(socket, io) );
-    // server.listen(port);
-    // if(development) console.log(`HTTP & SOCKET on port ${port}`);
+    const server = (0, http_1.createServer)(app);
+    const io = new socket_io_1.Server(server);
+    io.on('connection', (socket) => (0, sockets_1.default)(socket, io));
+    server.listen(PORT);
+    if (development)
+        console.log(`localhost${PORT}`);
 };
 exports.default = socket;
--- src/app/sockets.ts
+++ src/app/sockets.ts
@@ -9,17 +9,15 @@ const socket = (app: Express) => {
 
     const development = process.env.NODE_ENV === "development";
 
-    app.listen(PORT, () => development && console.log(`http://localhost:${PORT}`))
-
-    // const server = createServer(app);
+    const server = createServer(app);
     
-    // const io = new Server(server);
+    const io = new Server(server);
 
-    // io.on('connection', (socket) => sockets(socket, io) );
+    io.on('connection', (socket) => sockets(socket, io) );
 
-    // server.listen(port);
+    server.listen(PORT);
 
-    // if(development) console.log(`HTTP & SOCKET on port ${port}`);
+    if(development) console.log(`localhost${PORT}`);
 };
 
 export default socket;

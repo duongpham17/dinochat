23e8f4ad7ecaa49e7d2041d2ae07cc9b26c6f968
duongpham17 <duongpham17@icloud.com>
2023-04-30 17:07:30 +0100

    url

--- build/@environment/index.js
+++ build/@environment/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.development_frontend_url = exports.production_frontend_url = exports.website_url = exports.website_name = void 0;
 exports.website_name = "Dinochat";
-exports.website_url = "https://simulator-21b0.onrender.com";
-exports.production_frontend_url = "https://simulator-21b0.onrender.com";
+exports.website_url = "https://dinochat.onrender.com";
+exports.production_frontend_url = "https://dinochat.onrender.com";
 exports.development_frontend_url = "http://localhost:3000";
--- src/@environment/index.ts
+++ src/@environment/index.ts
@@ -1,7 +1,7 @@
 export const website_name = "Dinochat"
 
-export const website_url = "https://simulator-21b0.onrender.com"
+export const website_url = "https://dinochat.onrender.com"
 
-export const production_frontend_url = "https://simulator-21b0.onrender.com"
+export const production_frontend_url = "https://dinochat.onrender.com"
 
 export const development_frontend_url = "http://localhost:3000"
